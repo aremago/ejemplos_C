c2870e9891c622ca320fa45f4461398c20bf9295
aremago <aremago@outlook.com>
2017-07-02 03:43:09 -0500

    Ejercicio De los Ciclos

---
+++ ciclos/ciclos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ciclos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x, i, c;
+            int N = 5;
+            // Ciclo for
+            for(i=0; i>N;i++)
+            {
+                Console.WriteLine("Ingresa el valos");
+                x = Int32.Parse(Console.ReadLine());
+            }
+
+            // Ciclo do while
+            c = 0;
+            do
+            {
+                Console.WriteLine("Ingresa el valos");
+                x = Int32.Parse(Console.ReadLine());
+
+                c++;
+            } while (c<N);
+
+            // Ciclo while
+            int d = 0;
+            while (c < N) ;
+            {
+                Console.WriteLine("Ingresa el valos");
+                x = Int32.Parse(Console.ReadLine());
+
+                d++;
+            }
+        }
+
+
+    }
+}
