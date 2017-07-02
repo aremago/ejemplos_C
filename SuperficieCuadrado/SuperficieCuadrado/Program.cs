d5f90c856e14e7f8f73ee011af5da3499d346a5a
aremago <aremago@outlook.com>
2017-07-02 03:38:41 -0500

    Ejercicio Superficies

---
+++ SuperficieCuadrado/SuperficieCuadrado/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SuperficieCuadrado
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int superficie, perimetro, bas;
+            Console.WriteLine("Ingresa la base del Cuadrado mayor de cero y positivo");
+            bas = Int32.Parse(Console.ReadLine());
+
+            if ( bas <= 0 )
+            {
+                Console.WriteLine("Ingresa un numero adecuado");
+            }
+            else
+            {
+                superficie = bas * bas;
+                perimetro = bas + bas + bas + bas;
+                Console.WriteLine("La superficie del cuadrado de base " + bas + " es " + superficie  );
+                Console.WriteLine("El Perimetro del cuadrado de base " + bas + " es " + perimetro);
+            }
+          
+            
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
